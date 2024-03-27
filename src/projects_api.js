7d3f4550f8317f1a4edbd341cdaf7ccbf63215e2
disha7555 <190320107555.ce.disha.shah@gmailcom>
2024-03-27 12:22:22 +0530

    commit projects_api.js

---
+++ src/projects_api.js
@@ -0,0 +1,48 @@
+const projects=
+[
+	{	"id":1,
+		"name":"Contact Form",
+		"frontend-technologies":"HTML,CSS,Bootstrap",
+		"backend-technologies":"nodejs,expressjs,mongoose",
+		"link":"https://contact-form-nodejs-d.onrender.com/",
+		"image":
+    },
+
+	{	"id":2,
+		"name":"Restaurant Menu Website",
+		"frontend-technologies":"HTML,CSS,Bootstrap,ReactJS",
+		"backend-technologies":"none",
+		"link":"https://restaurant-menu-9t0x.onrender.com",
+		"image":
+    },
+	
+    {	"id":3,
+    "name":"Login-Register Form",
+    "frontend-technologies":"HTML,CSS,bootstrap,ReactJS",
+    "backend-technologies":"none",
+    "link":"https://login-register-form-new.onrender.com/",
+    "image":
+    },
+
+    {	"id":4,
+    "name":"Landing Page For Theka Coffee Brand",
+    "frontend-technologies":"HTML,CSS,ReactJS",
+    "backend-technologies":"none",
+    "link":"https://theka-coffee-landing-page.onrender.com",
+    "image":
+    },
+    {	"id":5,
+    "name":"Survey Project",
+    "frontend-technologies":"HTML,CSS,ReactJS",
+    "backend-technologies":"none",
+    "link":"https://survey-project-new.onrender.com",
+    "image":
+    },
+    {	"id":6,
+    "name":"Quiz App (with external API)",
+    "frontend-technologies":"HTML,CSS,ReactJS",
+    "backend-technologies":"none",
+    "link":"https://quiz-app-new.onrender.com",
+    "image":
+    }	
+]
