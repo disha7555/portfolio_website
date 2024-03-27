dd970a5caf63de145352f50e44302bb6656c3591
disha7555 <190320107555.ce.disha.shah@gmailcom>
2024-03-27 12:28:29 +0530

    commit apis

---
+++ src/miniprojects_api.js
@@ -0,0 +1,134 @@
+const miniprojects=
+[
+	{	"id":1,
+		"name":"TODO List(with add,delete)",
+		"frontend-technologies":"HTML,CSS,ReactJS",
+		"backend-technologies":"none",
+		"link":"https://todo-list-with-add-delete.onrender.com",
+		"image":
+      },
+
+	{	"id":2,
+		"name":"Online Calculator",
+		"frontend-technologies":"HTML,CSS,JavaScript",
+		"backend-technologies":"none",
+		"link":"https://online-calculator-x0b9.onrender.com/",
+		"image":
+      },
+	
+	  {	"id":3,
+		"name":"Captcha Generator",
+		"frontend-technologies":"HTML,CSS,JavaScript",
+		"backend-technologies":"none",
+		"link":"https://captcha-generator.onrender.com/",
+		"image":
+      },
+
+	  {	"id":4,
+		"name":"Feedback Website For Airflight",
+		"frontend-technologies":"HTML,CSS,JavaScript",
+		"backend-technologies":"none",
+		"link":"https://feedback-airflight-new.onrender.com/",
+		"image":
+      },
+	  {	"id":5,
+		"name":"TODO List(with add,edit,delete)",
+		"frontend-technologies":"HTML,CSS,ReactJS",
+		"backend-technologies":"none",
+		"link":"https://todo-list-jmdk.onrender.com",
+		"image":
+      },
+	  {	"id":6,
+	  "name":"Analog Clock",
+	  "frontend-technologies":"HTML,CSS,JavaScript",
+	  "backend-technologies":"none",
+	  "link":"https://analog-clock-dbob.onrender.com/",
+	  "image":
+	},
+	{"id":7,
+	  "name":"Form With Greeting Screen",
+	  "frontend-technologies":"ReactJS",
+	  "backend-technologies":"none",
+	  "link":"https://form-with-greeting-screen-with-react.onrender.com/",
+	  "image":
+	},	 
+	{"id":8,
+	  "name":"Slot Machine Game",
+	  "frontend-technologies":"HTML,CSS,ReactJS",
+	  "backend-technologies":"none",
+	  "link":"https://slot-machine-game-new.onrender.com",
+	  "image":
+	},	 
+	{"id":9,
+	  "name":"Digital Clock",
+	  "frontend-technologies":"HTML,CSS,ReactJS",
+	  "backend-technologies":"none",
+	  "link":"https://digital-clock-new.onrender.com",
+	  "image":
+	},	
+	{"id":10,
+	  "name":"Mini Receipes Website",
+	  "frontend-technologies":"HTML,CSS,Bootstrap,JavaScript",
+	  "backend-technologies":"none",
+	  "link":"https://mini-recipes-website.onrender.com/",
+	  "image":
+	},
+	{"id":11,
+	  "name":"Image Slider",
+	  "frontend-technologies":"HTML,CSS,JavaScript",
+	  "backend-technologies":"none",
+	  "link":"https://image-slider-new.onrender.com",
+	  "image":
+	},	
+	{"id":12,
+	  "name":"UseReducer Hooks",
+	  "frontend-technologies":"HTML,CSS,ReactJS",
+	  "backend-technologies":"none",
+	  "link":"https://usereducer-project.onrender.com",
+	  "image":
+	},	
+	{"id":13,
+	  "name":"UseState Hooks",
+	  "frontend-technologies":"HTML,CSS,ReactJS",
+	  "backend-technologies":"none",
+	  "link":"https://usestate-project.onrender.com/",
+	  "image":
+	},	 
+	{"id":14,
+	  "name":"UseEffect Hooks",
+	  "frontend-technologies":"HTML,CSS,ReactJS",
+	  "backend-technologies":"none",
+	  "link":"https://useeffect-project.onrender.com/",
+	  "image":
+	},	
+	{"id":15,
+	  "name":"Tell Me A Joke",
+	  "frontend-technologies":"HTML,CSS,JavaScript",
+	  "backend-technologies":"none",
+	  "link":"https://tell-me-a-joke-9vad.onrender.com/",
+	  "image":
+	},	
+	{"id":16,
+	  "name":"Change Background Color",
+	  "frontend-technologies":"HTML,CSS,JavaScript",
+	  "backend-technologies":"none",
+	  "link":"https://change-background-color.onrender.com/",
+	  "image":
+	},	 
+	{"id":17,
+	  "name":"Guess The Number",
+	  "frontend-technologies":"HTML,CSS,JavaScript",
+	  "backend-technologies":"none",
+	  "link":"https://guess-the-number-3xl1.onrender.com",
+	  "image":
+	},	
+	{"id":18,
+	  "name":"Snake Water Gun Game",
+	  "frontend-technologies":"HTML,CSS,JavaScript",
+	  "backend-technologies":"none",
+	  "link":"https://snake-water-gun-new.onrender.com",
+	  "image":
+	},	     
+]
+
+export default miniprojects;
--- src/projects_api.js
+++ src/projects_api.js
@@ -46,3 +46,5 @@ const projects=
     "image":
     }	
 ]
+
+export default projects;
